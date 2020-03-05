62b578368085f2c87eca1dfde38d124f99c15e55
Rémi Courvoisier <remi.courvoisier94@gmail.com>
2020-03-05 17:36:40 +0100

    Précision du model clients pour la validation

--- src/models/clients.js
+++ src/models/clients.js
@@ -12,23 +12,44 @@ module.exports = function(sequelize, DataTypes) {
 		Nom_Prenom: {
 			field : 'Nom_Prenom',
 			type: DataTypes.STRING(350),
-			allowNull: false
+			allowNull: false,
+			validate : {
+				notNull : {
+					msg : 'Le nom et prénom doivent être indiqués.'
+				}
+			}
 		},
 		Adresse_Facturation: {
 			field : 'Adresse_Facturation',
 			type: DataTypes.STRING(1000),
-			allowNull: false
+			allowNull: false,
+			validate : {
+				notNull : {
+					msg : 'L\'adresse de facturation doit être renseignée.'
+				}
+			}
 		},
 		Email: {
 			field : 'Email',
 			type: DataTypes.STRING(320),
-			allowNull: false
+			allowNull: false,
+			validate : {
+				isEmail: {
+					msg : 'L\'adresse e-mail est incorrecte.'
+				},
+				notNull : {
+					msg : 'L\'adresse e-mail doit être renseignée.'
+				}
+			}
 		},
 		Telephone: {
 			field : 'Telephone',
 			type: DataTypes.STRING(10),
 			allowNull: false,
 			validate : {
+				notNull : {
+					msg : 'Le numéro de téléphone doit être renseigné.'
+				},
                 isPhoneNumber : value => {
                     if(!value.match(/[0-9]{10}/)) {
                         throw new Error('Numéro de téléphone invalide')
@@ -39,13 +60,21 @@ module.exports = function(sequelize, DataTypes) {
 		Type: {
 			field : 'Type',
 			// type: DataTypes.STRING(13),
-			type : DataTypes.ENUM,
-            values : ['Professionnel', 'Particulier'],
-			allowNull: false
+			type : DataTypes.ENUM('Professionnel', 'Particulier'),
+			allowNull: false,
+			validate : {
+				notNull : {
+					msg : 'Le type doit être renseigné.'
+				},
+				isIn : {
+					args : [['Professionnel', 'Particulier']],
+					msg : 'Le type doit être Professionnel ou Particulier'
+				}
+			}
 		},
 		Nombre_Prestations: {
 			field : 'Nombre_Prestations',
-			type: DataTypes.INTEGER(4),
+			type: DataTypes.INTEGER(4).UNSIGNED,
 			allowNull: false,
 			defaultValue: '0'
 		},
@@ -57,7 +86,7 @@ module.exports = function(sequelize, DataTypes) {
 		},
 		Paiement_En_Retard: {
 			field : 'Paiement_En_Retard',
-			type: DataTypes.INTEGER(4),
+			type: DataTypes.INTEGER(4).UNSIGNED,
 			allowNull: false,
 			defaultValue: '0'
 		}
