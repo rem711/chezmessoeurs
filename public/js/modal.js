69f1bb9a4ca2a89128af10e27efe8b908c77d41c
rem711 <remi.courvoisier94@gmail.com>
2020-03-06 01:04:30 +0100

    Ajout d'éléments pour la gestion de modals, début d'utilisation dans la ve clients

---
+++ public/js/modal.js
@@ -0,0 +1,27 @@
+// Get the modal
+var modal = document.getElementById("modalUpdate");
+            
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var modalCloseElmt = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+for(let i = 0; i < modalCloseElmt.length; i++) {
+    modalCloseElmt[i].onclick = function() {
+    modal.style.display = "none";
+    }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
