0519231d5a9d6af3aa771c76e3d45de581064e79
rem711 <remi.courvoisier94@gmail.com>
2020-03-24 17:36:47 +0100

    correction de l'issue n'°#1. Les lignes sélectionnées peuvent être désélectionnées.

--- public/js/devis.js
+++ public/js/devis.js
@@ -16,13 +16,18 @@ const btnArchiveDevis = document.getElementById('btnArchiveDevis')
 const toggleSelectedTr = (trClicked) => {
     // si une précédente sélectionnée
     let trSelected = document.getElementsByClassName('selected')[0]
+    if(trClicked !== trSelected) {
+        if(trSelected) {
+            // on retire la classe
+            trSelected.setAttribute('class', '')
+        }
+        // on assigne à la nouvelle tr sélectionnée la classe
+        trClicked.setAttribute('class', 'selected')
+    }
     if(trSelected) {
-        // on retire la classe
+        // on retire la classe s'il y avait une précédente tr de sélectionnée
         trSelected.setAttribute('class', '')
     }
-    // on assigne la nouvelle tr sélectionnée et on lui ajoute la classe
-    trSelected = trClicked
-    trSelected.setAttribute('class', 'selected')
 }
 
 const newDevis = () => {
